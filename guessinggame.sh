21a77f698b52cc0cf8875c0b57f0ce3adc289a62
test10-mt <61598118+test10-mt@users.noreply.github.com>
2020-02-29 22:46:32 +0530

    Add files via upload

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,6 +1,6 @@
 countfiles()
 {
-    count=$(ls -l | grep "^[d-]" | wc -l)
+	count=$(ls -la | grep ^- | wc -l)
     #echo "NUMBER OF FILE CHECK $count"
 }
 
@@ -8,21 +8,18 @@ countfiles
 
 echo "GUESS THE NUMBER OF FILES IN THE CORRECT DIRECTORIES"
 read response
-#echo "your response is $response"
+echo "your response is $response"
 
 while [[ $count != $response ]]
 do
-    #echo "--first check"
-    #echo "count    $count"
-    #echo "response $response"
-    if [[ $count >  $response ]]
+    if [[ $count -ge  $response ]]
     then
-        echo "Your guess is too low. Try again:"
+        echo "Your guess is LOW. Try again:"
         read response
     fi
-    if [[ $count < $response ]]
+    if [[ $count -lt $response ]]
     then
-        echo "Your guess is too high. Try again:"
+        echo "Your guess is HIGH. Try again:"
         read response
     fi
 done
--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 echo "GUESSS THE NUMBER OF DIRECTOIRES" >> README.md
 echo "CURRENT DATE AND TIME IS" >> README.md
 date >> README.md
-echo "Number of files contained in guessinggame.sh is \n"  >> README.md
+echo "Number of files contained in guessinggame.sh is"  >> README.md
 grep -c '' guessinggame.sh >> README.md
